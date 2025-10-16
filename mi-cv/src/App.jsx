92605e89e49ee9d0a35a8fd0a1d27214d5440ac1
Juanma2111 <72576014+Juanma2111@users.noreply.github.com>
2025-10-16 20:58:58 +0200

    Inicio CV Online

---
+++ mi-cv/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-blue-500">
+        ¡Hola Juanma! 🚀
+      </h1>
+      <p className="text-gray-600 mt-4">Tailwind está funcionando 🎨</p>
+      <p className="mt-4 font-bold">venga a trabaja</p>
+    </div>
+  );
+}
+
+export default App;
